5ee57506175fe2986d6a1f4f8e9fd2c7fb1fb458
Felix K Friju <66660551+SaitamaOPM99@users.noreply.github.com>
2025-04-07 23:49:35 +0530

    Update text.go

--- config/text.go
+++ config/text.go
@@ -19,7 +19,8 @@ var Commands map[string]string = map[string]string{
 	"ABOUT": `
 ○ <b>Language</b>: <a href='https://go.dev'>GO</a>
 ○ <b>Library</b>: <a href='https://github.com/PaulSonOfLars/gotgbot'>GoTgbot</a>
-○ <b>Support</b>: <a href='https://t.me/FractalProjects'>@Fractal</a>
+○ <b>Support</b>: <a href='https://t.me/AnimeXSaga'>@AnimeXSaga</a>
+○ <b>Owner</b>: <a href='https://t.me/MalluSaitama'>@MalluSaitama</a>
 	`,
 
 	"HELP": `
