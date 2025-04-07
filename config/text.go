d94acad3e724679cb8bf872347af48a0a3017fea
Felix K Friju <66660551+SaitamaOPM99@users.noreply.github.com>
2025-04-07 23:50:05 +0530

    Update text.go

--- config/text.go
+++ config/text.go
@@ -13,7 +13,7 @@ import (
 // Standard command replies. Add a new entry to create new command no extra configs needed.
 var Commands map[string]string = map[string]string{
 	"START": `
-<i>👋 Hi <b>{mention}</b> I work for <b>@AnimeXSaga</b>! To access all our exclusive content, you <b>must</b> join 🚨 our backup channel — it's where the magic continues! 🌟
+<i>👋 Hi <b>{mention}</b>,  I work for <b>@AnimeXSaga</b>! To access all our exclusive content, you <b>must</b> join 🚨 our backup channel — it's where the magic continues! 🌟
 <b>Learn</b> how to use me 👉 /help to easily <b>share</b> content with anyone 👣 </i>
 `,
 	"ABOUT": `
