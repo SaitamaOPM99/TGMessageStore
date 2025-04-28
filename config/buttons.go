4b93b2d1d42530224c50a28b615e7f2f2018910d
Felix K Friju <66660551+SaitamaOPM99@users.noreply.github.com>
2025-04-28 23:20:26 +0530

    Update buttons.go

--- config/buttons.go
+++ config/buttons.go
@@ -7,14 +7,14 @@ package config
 import "github.com/PaulSonOfLars/gotgbot/v2"
 
 var Buttons map[string][][]gotgbot.InlineKeyboardButton = map[string][][]gotgbot.InlineKeyboardButton{
-	"START": {{aboutButton, helpButton}, {{Text: "ᴄᴏɴɴᴇᴄᴛ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ", Url: "https://tg-message-store.vercel.app"}}},
-	"ABOUT": {{homeButton, helpButton}, {{Text: "Source 🔗", Url: "https://github.com/Jisin0/TGMessageStore"}}},
-	"HELP":  {{aboutButton, homeButton}},
+	"START": {{aboutButton, helpButton}},
+	"ABOUT": {{}},
+	"HELP":  {{}},
 }
 
 // Single buttons used to build composite markups.
 var (
-	aboutButton = gotgbot.InlineKeyboardButton{Text: "About ℹ️", CallbackData: "cmd_ABOUT"}
-	helpButton  = gotgbot.InlineKeyboardButton{Text: "Help ❓", CallbackData: "cmd_HELP"}
+	aboutButton = gotgbot.InlineKeyboardButton{Text: "✨", CallbackData: "cmd_ABOUT"}
+	helpButton  = gotgbot.InlineKeyboardButton{Text: "⚡", CallbackData: "cmd_HELP"}
 	homeButton  = gotgbot.InlineKeyboardButton{Text: "Home 🏠", CallbackData: "cmd_START"}
 )
