6d7a0f405d77c491f8fc1ddc1cde9558362437ae
Felix K Friju <66660551+SaitamaOPM99@users.noreply.github.com>
2025-04-28 23:20:05 +0530

    Update text.go

--- config/text.go
+++ config/text.go
@@ -13,27 +13,14 @@ import (
 // Standard command replies. Add a new entry to create new command no extra configs needed.
 var Commands map[string]string = map[string]string{
 	"START": `
-<i>👋 Hi <b>{mention}</b> I'm a bot that can create <b>links</b> to <b>store</b> your <b>files and messages</b> in a single click 🔗
-<b>Learn</b> how to use me 👉 /help to easily <b>share</b> content with anyone 👣 </i>
+<i> Hai, I'm a file store bot that works for <a href="https://t.me/AnimeXSaga">AɴɪᴍᴇXSᴀɢᴀ</a>  👣 </i>
 `,
 	"ABOUT": `
-○ <b>Language</b>: <a href='https://go.dev'>GO</a>
-○ <b>Library</b>: <a href='https://github.com/PaulSonOfLars/gotgbot'>GoTgbot</a>
-○ <b>Support</b>: <a href='https://t.me/FractalProjects'>@Fractal</a>
+<i>😐 Sorry dude <b>only</b> an <b>admin</b> can do that !</i>
 	`,
 
 	"HELP": `
-<i>👋 Hey {mention} I'm a bot that can create <b>permanent</b> links to a single or a <b>batch</b> of messages.</i>
-<i><b>Add</b> me to your <b>channel</b> where you'll posting all the content before using me 😉</i>
-
-<i>Here's a list of my available commands 👉</i>
-
-/start : Start the bot.
-/batch : Create a new message batch.
-/genlink : Create link for a single post.
-/about : Get some data about the bot.
-/help  : Display this help message.
-/privacy: Learn how this bot uses your data.
+ <i>😐 Sorry dude <b>only</b> an <b>admin</b> can do that !</i>
 `,
 
 	"PRIVACY": `<i>This bot does not connect to any database and hence <b>does not store any user data</b> in any form.</i>`,
