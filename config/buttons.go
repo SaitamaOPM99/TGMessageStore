7e81b95917a26b6248365972836f103d9bd26b33
Felix K Friju <66660551+SaitamaOPM99@users.noreply.github.com>
2025-05-01 21:01:39 +0530

    Update buttons.go

--- config/buttons.go
+++ config/buttons.go
@@ -15,6 +15,6 @@ var Buttons map[string][][]gotgbot.InlineKeyboardButton = map[string][][]gotgbot
 // Single buttons used to build composite markups.
 var (
 	aboutButton = gotgbot.InlineKeyboardButton{Text: "➕", CallbackData: "cmd_ABOUT"}
-	helpButton  = gotgbot.InlineKeyboardButton{Text: "🆗", CallbackData: "cmd_HELP"}
+	helpButton  = gotgbot.InlineKeyboardButton{Text: "✅", CallbackData: "cmd_HELP"}
 	homeButton  = gotgbot.InlineKeyboardButton{Text: "Home 🏠", CallbackData: "cmd_START"}
 )
