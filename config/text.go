b37c19fcacb48462292bc8ce56a87323c754c2b2
Felix K Friju <66660551+SaitamaOPM99@users.noreply.github.com>
2025-05-16 10:18:00 +0530

    Update text.go

--- config/text.go
+++ config/text.go
@@ -13,7 +13,7 @@ import (
 // Standard command replies. Add a new entry to create new command no extra configs needed.
 var Commands map[string]string = map[string]string{
 	"START": `
-<b> Hello, I am a File Store Bot Powered by @AnimeXSaga ⚡️ </b> 
+<b> Hello, I am a File Store Bot Powered by @AnimeXSaga ⛩️ </b> 
 `,
 	"ABOUT": `
 ○ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/SAGA_UPDATES'>SAGA_UPDATES</a>
